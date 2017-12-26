023a7276c257b819f8e8d38cac4cf9b1aca7b0a2
Khan <m.pizner@gmail.com>
2017-12-26 20:10:30 +0100

    Uloha1

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 import requests
 import json
-from collections import defaultdict
 
 uctovne_jednotky_url = 'http://www.registeruz.sk/cruz-public/api/uctovne-jednotky'
 uctovna_jednotka_url = 'http://www.registeruz.sk/cruz-public/api/uctovna-jednotka'
@@ -8,6 +7,9 @@ uctovna_zavierka_url = 'http://www.registeruz.sk/cruz-public/api/uctovna-zavierk
 
 uctovne_vykazy_url = 'http://www.registeruz.sk/cruz-public/api/uctovne-vykazy'
 uctovny_vykaz_url = 'http://www.registeruz.sk/cruz-public/api/uctovny-vykaz'
+
+
+
 
 
 def rest_request(url, params):
@@ -24,7 +26,7 @@ def uloha1():
 
     data = rest_request(uctovne_jednotky_url, parm)
 
-    output_str = defaultdict(list)
+    output_str = {}
     output = False
 
     for key in data['id']:
@@ -47,21 +49,52 @@ def uloha1():
     if output == False:
         print("prazdny vystup")
     else:
-       # sorted(output_str, key=lambda d: tuple(map(int, d[0].split('-'))))
-        output_str.sort(key=lambda r: r[0])
         for out in output_str:
-            print(str(out[0]) + "\t" + str(out[1]))
+            print(str(out) + "\t" + str(output_str[str(out)]))
+
+
+def uloha2_one_page(data):
+    if 'id' in data:
+        for key in data['id']:
+            #print(str(key))
+
+            parm = {'id': str(key)}
+            data2 = rest_request(uctovny_vykaz_url, parm)
+
+            if 'idUctovnejZavierky' not in data2:
+                continue
+
+            parm2 = {'id':data2['idUctovnejZavierky']}
+            data3 = rest_request(uctovna_zavierka_url, parm2)
+
+            date = data3['datumZostaveniaK']
+            if 'ico' in data3:
+                print(str(key) + "\t" + str(data3['ico'])+ "\t" + str(date))
+            else:
+                parm3 = {'id': data3['idUJ']}
+                data4 = rest_request(uctovna_jednotka_url,parm3)
+                if 'ico' in data4:
+                    print(str(key) + "\t" + str(data4['ico']) + "\t" + str(date))
+
 
 
 def uloha2():
-    parm = {'zmenene-od': input("Enter date:")}
+    input_str = input("Enter date:")
+    parm = {'max-zaznamov': '1000', 'zmenene-od': input_str }
     data = rest_request(uctovne_vykazy_url, parm)
+    uloha2_one_page(data)
 
-    for key in data['id']:
-        print(str(key))
-        parm = {'id': str(key)}
-        data2 = rest_request(uctovny_vykaz_url, parm)
+    while 'pocetZostavajucichId' in data:
+        #count = data['pocetZostavajucichId']
+        l = len(data['id'])-1
+        last = data['id'][l]
+        parm = {'max-zaznamov': '1000', 'pokracovat-za-id': last, 'zmenene-od': input_str}
+        data = rest_request(uctovne_vykazy_url,parm)
+        uloha2_one_page(data)
 
 
-uloha1()
-# uloha2()
+#uloha1()
+uloha2()
+
+if __name__ == "__main__":
+    uloha1()
